f30001b660c23ad478b501175da7316dcd10c247
AmirPaknahad <ar.pknhd@gmail.com>
2024-03-01 20:55:05 +0330

    test1 completed

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -11,7 +11,7 @@ public class Exercise3 {
     */
 
     public static String extractURL(String text) {
-        String regex = "write your regex pattern here!";  // TODO
+        String regex = "(https?)://[a-zA-Z0-9-]*[.]?\\w*[.]\\w{2,3}";  // TODO
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
